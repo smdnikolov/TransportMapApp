b333eae7a8800fc9a323afde3d9df87efdd65989
smdnikolov <smdnikolov@gmail.com>
2021-02-03 11:04:24 +0200

    Added map

---
+++ src/components/map/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './styles.css'
+
+const Map = () => {
+
+
+    return <div >
+        <h1>The map</h1>
+        <div id="mapid">
+            <MapContainer center={[42.697708, 23.321867]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {/* <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+                </Marker> */}
+            </MapContainer>
+        </div>
+    </div>
+
+
+}
+export default Map
