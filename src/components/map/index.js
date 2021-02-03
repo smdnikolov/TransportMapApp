26215c008f00b580a409f5125463d4648ffee260
smdnikolov <smdnikolov@gmail.com>
2021-02-03 11:54:52 +0200

    Added stops
    Added stops for first route on map

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './styles.css'
+import data from '../../data.json'
 
 const Map = () => {
 
 
+    const stops = data[0].stops
+    console.log(stops)
+    let z = stops.map(x => {
+        console.log(x.location)
+        return <Marker position={[x.location.lat, x.location.lng]} key={x.id}>
+            <Popup>
+                {x.name}
+            </Popup>
+        </Marker>
+    })
+    console.log(stops)
     return <div >
         <h1>The map</h1>
         <div id="mapid">
-            <MapContainer center={[42.697708, 23.321867]} zoom={23} scrollWheelZoom={false}>
+            <MapContainer center={[42.697708, 23.321867]} zoom={18} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -16,8 +28,9 @@ const Map = () => {
                 <Marker position={[42.697708, 23.321867]}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+                    </Popup>
                 </Marker>
+                {z}
             </MapContainer>
         </div>
     </div>
